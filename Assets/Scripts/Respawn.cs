dcac5124a274b070737e4567a78e7f910ade3869
Birgitte <birgitte.wintersto@outlook.com>
2020-06-07 18:15:22 +0200

    Added death/respawn +  Material

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField]Transform respawnPoint; 
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("Player"))
+        {
+            col.transform.position = respawnPoint.position;
+        }
+    }
+}
